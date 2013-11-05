ff432c92cca2e38e166bd9f960c14bd5d1d12f3a
Andre-Patrick Bubel <code@andre-bubel.de>
2013-11-05 01:48:04 +0100

    add license text

--- digiabo.py
+++ digiabo.py
@@ -1,12 +1,26 @@
 """
 Taz digiabo download link generator
 
-Usage: digiabo.py [-d N | --days N]
+Usage: digiabo.py [-d N | --days N] | --license
 
 Options:
     -d N --days N  number of days in the past to generate download links for [default: 1]
-
+    --license  Print the license of this script
 """
+
+__license__ = """Taz digiabo download link generator
+
+Copyright (c) 2013, Andre-Patrick Bubel <code@andre-bubel.de>
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+    Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+    Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
 from datetime import date, timedelta
 from docopt import docopt
 
@@ -60,6 +74,10 @@ def print_templates(this_date):
 def main():
     arguments = docopt(__doc__, version="0.0.1")
     dates = past_dates(int(arguments["--days"]))
+
+    if arguments["--license"]:
+        print __license__
+        exit(0)
 
     for d in dates:
         print_templates(d)
